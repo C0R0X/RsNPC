0edadcb82957adeb9d45b5e659207a2c8ce15a36
C0R0X <constantinfrank2005@gmail.com>
2024-08-18 00:02:33 +0200

    fixes

---
+++ src/main/java/com/smallaswater/npc/event/PlayerClickOnNpcEvent.java
@@ -0,0 +1,10 @@
+package com.smallaswater.npc.event;
+
+import cn.nukkit.event.Cancellable;
+import cn.nukkit.event.Event;
+
+public class PlayerClickOnNpcEvent extends Event implements Cancellable {
+
+
+
+}
